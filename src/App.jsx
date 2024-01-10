db899f92802b43e52ae849e8781cdf10ae082a9f
akash <musaddekurakash@gmail.com>
2024-01-11 01:59:21 +0600

    first two button added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+    <button className='border bg-white h-12 w-12 text-lg border-gray-400 m-1 leading-9'>X</button>
+    <button className='border bg-white h-12 w-12 text-lg border-gray-400 m-1 leading-9'>X</button>
+    </>
+  );
+};
+
+export default App;
