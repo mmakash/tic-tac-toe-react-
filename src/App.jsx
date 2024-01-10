60b5300019383ca720b56c0ae292150565d661a5
akash <musaddekurakash@gmail.com>
2024-01-11 02:32:55 +0600

    value added to squire component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const App = () => {
+function Squire({value}) {
+  return (
+    <button className="border bg-white h-12 w-12 text-lg border-gray-400 m-1 leading-9">
+      {value}
+    </button>
+  );
+}
+
+const Board = () => {
   return (
     <>
-    <button className='border bg-white h-12 w-12 text-lg border-gray-400 m-1 leading-9'>X</button>
-    <button className='border bg-white h-12 w-12 text-lg border-gray-400 m-1 leading-9'>X</button>
+      <div>
+        <Squire value={1}/>
+        <Squire value={2}/>
+        <Squire value={3}/>
+      </div>
+
+      <div>
+        <Squire value={4}/>
+        <Squire value={5}/>
+        <Squire value={6}/>
+      </div>
+
+      <div>
+        <Squire value={7}/>
+        <Squire value={8}/>
+        <Squire value={9}/>
+      </div>
     </>
   );
 };
 
-export default App;
+export default Board;
