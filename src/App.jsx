9f83b6511fb4eb020c1c8af10fda9eac23c7bbb2
akash <musaddekurakash@gmail.com>
2024-01-11 14:42:04 +0600

    calculate winner completed

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,39 @@ function Squire({ value, onSquireClick }) {
 const Board = () => {
 
   const [squire, setSquire] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squire);
+  let status;
+
+  if(winner){
+    status = "Winner: " + winner;
+  }
+  else{
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   const handleClick = (i) => {
-
+    if(squire[i] || calculateWinner(squire)){
+      return;
+    }
     const nextSquire = squire.slice();
     nextSquire[i] = "X";
+
+    if(xIsNext){
+      nextSquire[i] = "X";
+    }
+    else{
+      nextSquire[i] = "O";
+    }
+
     setSquire(nextSquire);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="flex">
         <Squire value={squire[0]} onSquireClick={() => handleClick(0)}/>
         <Squire value={squire[1]} onSquireClick={() => handleClick(1)}/>
@@ -44,3 +67,27 @@ const Board = () => {
 };
 
 export default Board;
+
+// ei function ti squire state ta ke parameter hishebee nicche
+// tarpor eti check korbe amader line e same element ase ki nei
+// same element thakle function ti shetike return korbe
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
